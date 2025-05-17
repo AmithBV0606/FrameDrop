94b3cf1404e5113ff2958344bfe225d972c7b4db
Amith B V <amithrao0606@gmail.com>
2025-05-17 07:06:42 +0530

    Again fixed DaisyUI's Drawer component height issue.

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,35 +1,3 @@
-// import MainDrawer from "@/components/MainDrawer";
-// import Navbar from "@/components/Navbar";
-// import { Roboto } from "next/font/google";
-
-// const roboto = Roboto({
-//   weight: "400",
-//   subsets: ["latin"],
-// });
-
-// export default function AppLayout({
-//   children,
-// }: Readonly<{
-//   children: React.ReactNode;
-// }>) {
-//   return (
-//     <html lang="en">
-//       <body className={`${roboto.className} antialiased`}>
-//         <Navbar />
-
-//         <main className="flex">
-//           {/* Drawer for large screen : */}
-//           <div className="grow-[25vw]">
-//             <MainDrawer />
-//           </div>
-
-//           <div className="grow-[75vw] w-full">{children}</div>
-//         </main>
-//       </body>
-//     </html>
-//   );
-// }
-
 import MainDrawer from "@/components/MainDrawer";
 import Navbar from "@/components/Navbar";
 import { Roboto } from "next/font/google";
@@ -45,8 +13,8 @@ export default function AppLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className="h-full">
-      <body className={`${roboto.className} antialiased h-full`}>
+    <html lang="en">
+      <body className={`${roboto.className} antialiased`}>
         <div className="flex flex-col h-full">
           <Navbar />
           <main className="flex flex-1">
